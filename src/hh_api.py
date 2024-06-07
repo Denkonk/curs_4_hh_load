9249834ef25ed340de9a4db7e342b45f5310c9a5
dkonkov <ferz5566@yandex.ru>
2024-06-07 15:52:37 +0300

    update hh_api.py

--- src/hh_api.py
+++ src/hh_api.py
@@ -7,17 +7,19 @@ class HH_API(Abstract_hh_API):
         self.params = {
             'text': '',
             'page': 0,
-            'per_page': 200
+            'per_page': 100,
+            'only_with_salary': True,
+            'area': 113
         }
 
     def get_vacancies(self, keyword, count):
         self.params.update({'text': keyword})
         response = requests.get(self.url, params=self.params)
-        return response
+        return response.json()['items']
 
-if __name__ == '__main__':
-    my_api = HH_API()
-    response = my_api.get_vacancies('водитель', 100)
-    print(response)
+# if __name__ == '__main__':
+#     my_api = HH_API()
+#     response = my_api.get_vacancies('водитель', 100)
+#     print(response)
 
 
