6524223fb84f56a12c0c721df847606870bedcda
dkonkov <ferz5566@yandex.ru>
2024-06-07 15:52:47 +0300

    update abstract_json_worker.py

--- src/abstract_json_worker.py
+++ src/abstract_json_worker.py
@@ -1,5 +1,6 @@
 from abc import ABC, abstractmethod
 
+#изменить название на большие буквы без подчеркивания
 class Abstract_json_worker(ABC):
     @abstractmethod
     def add_vacancies(self, vacancies):
